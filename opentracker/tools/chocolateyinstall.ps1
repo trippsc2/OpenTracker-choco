87c17edac871511434e1a977a2ac3b57ef7e0dbe
trippsc2 <trippsc2@github.com>
2021-02-07 09:34:56 -0500

    Fix Chocolatey package to remove unnecessary dependencies and 64-bit processes.

--- opentracker/tools/chocolateyinstall.ps1
+++ opentracker/tools/chocolateyinstall.ps1
@@ -1,18 +1,14 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 $url        = 'https://github.com/trippsc2/OpenTracker/releases/download/1.7.0/OpenTracker.1.7.0.win-x86.msi'
-$url64      = 'https://github.com/trippsc2/OpenTracker/releases/download/1.7.0/OpenTracker.1.7.0.win-x86.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
   url           = $url
-  url64bit      = $url64
   softwareName  = 'opentracker*'
   checksum      = '508EA16CB22871DEC61D4C4BE252F0C028ED3069080F65B2C9C0B741651E5323'
   checksumType  = 'sha256'
-  checksum64    = '508EA16CB22871DEC61D4C4BE252F0C028ED3069080F65B2C9C0B741651E5323'
-  checksumType64= 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
 }
