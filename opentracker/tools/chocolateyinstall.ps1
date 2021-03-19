03ae136aeee18fd07b22e582194f634d6416480c
trippsc2 <trippsc2@github.com>
2021-03-18 20:50:51 -0400

    Update package to v1.8.2.

--- opentracker/tools/chocolateyinstall.ps1
+++ opentracker/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$url        = 'https://github.com/trippsc2/OpenTracker/releases/download/1.8.1/OpenTracker.1.8.1.win-x86.msi'
+$url        = 'https://github.com/trippsc2/OpenTracker/releases/download/1.8.2/OpenTracker.1.8.2.win-x86.msi'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   fileType      = 'msi'
   url           = $url
   softwareName  = 'opentracker*'
-  checksum      = 'DBD4A3EBC44171F3AE6C945218529266C918D85D43F6972B634ADCCBF020D625'
+  checksum      = 'FA7F14C401B6F73A32290CD8E7B14EDD54CE59044B61F7D4998BBA2CC393FE34'
   checksumType  = 'sha256'
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
   validExitCodes= @(0, 3010, 1641)
